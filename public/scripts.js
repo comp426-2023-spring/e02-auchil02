2932e5bd6a850bab9dac841e79481cbb21103783
auchil02 <auchil@ad.unc.edu>
2023-05-09 17:47:54 -0400

    create rules.html, add functions to index.html and scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,20 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+function resetChoices() {
+    // Reset game type radio buttons
+    document.getElementById("rps").checked = false;
+    document.getElementById("rpsls").checked = false;
+  
+    // Reset opponent checkbox
+    document.getElementById("opponent").checked = false;
+  
+    // Reset shots radio buttons
+    document.getElementById("rock").checked = false;
+    document.getElementById("paper").checked = false;
+    document.getElementById("scissors").checked = false;
+    document.getElementById("lizard").checked = false;
+    document.getElementById("spock").checked = false;
+  }
+  
